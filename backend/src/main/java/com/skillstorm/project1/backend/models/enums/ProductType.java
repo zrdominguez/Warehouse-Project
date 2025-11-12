6b52a33d360c90144cd284eff42e1b07864caa68
zrdominguez <zechariahd@gmail.com>
2025-11-12 10:04:59 -0600

    Created Enum classes to hold the allowed types for products and warehouses

---
+++ backend/src/main/java/com/skillstorm/project1/backend/models/enums/ProductType.java
@@ -0,0 +1,9 @@
+package com.skillstorm.project1.backend.models.enums;
+
+public enum ProductType {
+    OTHER,
+    STRING,
+    WOODWIND,
+    BRASS,
+    PERCUSSION
+}
---
+++ backend/src/main/java/com/skillstorm/project1/backend/models/enums/WarehouseType.java
@@ -0,0 +1,5 @@
+package com.skillstorm.project1.backend.models.enums;
+
+public enum WarehouseType {
+    INSTRUMENTS,
+}
