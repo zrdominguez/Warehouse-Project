f92bed53c1f9db4387f905c5d91c75f58a8f462f
zrdominguez <zechariahd@gmail.com>
2025-11-12 16:00:42 -0600

    created an addProduct and removeProduct method to test for later. Also add FK to connect to sections_products table

---
+++ backend/src/main/java/com/skillstorm/project1/backend/models/Section.java
@@ -0,0 +1,117 @@
+package com.skillstorm.project1.backend.models;
+
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "SECTIONS")
+public class Section {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "warehouse_id")
+    private Warehouse warehouse;
+
+    //Might have to use a Enum for this or somehow
+    //get it to relate to the product type
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "section", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<SectionProduct> sectionProducts = new HashSet<>();
+    
+    public Section() {
+    }
+
+    public Section(Warehouse warehouse, String name) {
+        this.warehouse = warehouse;
+        this.name = name;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Warehouse getWarehouse() {
+        return warehouse;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) return true;
+        if(!(obj instanceof Section)) return false;
+        Section other = (Section) obj;
+        return id == other.id;
+    }
+
+    @Override
+    public String toString() {
+        return "Section [id=" + id + ", name=" + name + ", createdAt=" + createdAt + "]";
+    }
+
+    public void addProduct(Product product, int quantity){
+        SectionProduct sectionProduct = new SectionProduct(this, product, quantity);
+        sectionProducts.add(sectionProduct);
+        product.getSectionProducts().add(sectionProduct);
+    }
+
+    public void removeProduct(Product product){
+        sectionProducts.removeIf(sectionProduct -> sectionProduct.getProduct().equals(product));
+        product.getSectionProducts().removeIf(sectionProduct -> sectionProduct.getSection().equals(this));
+    }
+}
