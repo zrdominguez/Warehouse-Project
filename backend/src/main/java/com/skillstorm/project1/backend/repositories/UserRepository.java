d1af40410d0a7e2e320e1c44f905af485dbe706d
zrdominguez <zechariahd@gmail.com>
2025-11-13 15:47:44 -0600

    created a test find userById method

--- backend/src/main/java/com/skillstorm/project1/backend/repositories/UserRepository.java
+++ backend/src/main/java/com/skillstorm/project1/backend/repositories/UserRepository.java
@@ -1,12 +1,14 @@
 package com.skillstorm.project1.backend.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.skillstorm.project1.backend.models.User;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
 
-    
+    Optional<User> findById(int id);
+
 }
