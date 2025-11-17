a80c2b386d70c0c58765d77d5211b758c9cd6cd5
zrdominguez <zechariahd@gmail.com>
2025-11-17 16:28:57 -0600

    deleted previous Optional logic

--- backend/src/main/java/com/skillstorm/project1/backend/repositories/ProductRepository.java
+++ backend/src/main/java/com/skillstorm/project1/backend/repositories/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.skillstorm.project1.backend.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,4 @@ import com.skillstorm.project1.backend.models.Product;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
-    
 }
