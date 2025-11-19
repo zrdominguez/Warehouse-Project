0e9afb94cb69a879e79c525ea6acbb9243491fb5
zrdominguez <zechariahd@gmail.com>
2025-11-19 15:16:51 -0600

    Used sample data to test both my warehouse list, cards and carousel

---
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,28 @@
+import WarehouseCarousel from "../WarehouseCarousel";
+import WarehouseList from "../WarehouseList";
+import "./LandingPage.css";
+
+const sample = [
+  { id: 1, name: "Dallas Warehouse", location: "Texas", capacity: 5000 },
+  { id: 2, name: "Chicago Storage", location: "Illinois", capacity: 8000 },
+  { id: 3, name: "Orlando Hub", location: "Florida", capacity: 3000 },
+  { id: 4, name: "Dallas Warehouse", location: "Texas", capacity: 5000 },
+  { id: 5, name: "Chicago Storage", location: "Illinois", capacity: 8000 },
+  { id: 6, name: "Orlando Hub", location: "Florida", capacity: 3000 },
+  { id: 7, name: "Dallas Warehouse", location: "Texas", capacity: 5000 },
+  { id: 8, name: "Chicago Storage", location: "Illinois", capacity: 8000 },
+  { id: 9, name: "Orlando Hub", location: "Florida", capacity: 3000 },
+  { id: 10, name: "Dallas Warehouse", location: "Texas", capacity: 5000 },
+  { id: 11, name: "Chicago Storage", location: "Illinois", capacity: 8000 },
+  { id: 12, name: "Orlando Hub", location: "Florida", capacity: 3000 },
+];
+
+export default function LandingPage() {
+  return (
+    <div className="w-screen flex flex-col justify-center gap-20">
+      <WarehouseCarousel warehouses={sample} direction={""}/>
+      <WarehouseCarousel warehouses={sample} direction={"reverse"}/>
+    </div>
+
+  )
+}
